baa1a0095e06698f5a0eea00c7e2125357ce77c3
itair <itair@163.com>
2013-12-23 21:40:27 +0800

    第四章
    复合类型，，估计今天做不完

---
+++ Try12232/try.cpp
@@ -0,0 +1,24 @@
+//C++ Primer Plus 课后习题 p109 3.7 
+// by itAir
+// Try to use Google C++ programing style
+// dir2/foo2.h（优先位置，详情如下）
+//C 系统文件
+//        C++系统文件
+//        其他库头文件
+//        本项目内头文件
+#include "try.h"
+#include <iostream>
+using namespace std;
+
+void main(){
+	cout<< "C++_Primer_Plus"<<endl;
+	cout<<"    by itAir"<<endl;        
+	cout<<"Hello World!"<<endl;
+	cout<<"_________________________________"<<endl<<endl;        
+	//1
+
+
+
+
+	system("pause");
+}
