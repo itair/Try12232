7519f6d72e320457944f2c58a885043bbb0c4426
itair <itair@163.com>
2013-12-24 14:22:18 +0800

    第一题

--- Try12232/try.cpp
+++ Try12232/try.cpp
@@ -16,6 +16,25 @@ void main(){
 	cout<<"Hello World!"<<endl;
 	cout<<"_________________________________"<<endl<<endl;        
 	//1
+	char name_first[20], name_last[10];
+	char grade;
+	int age;
+	cout<<" What is your first name? ";
+	cin.getline(name_first,20);
+	cout<<" What is your last name? ";
+	cin.getline(name_last,10);
+	cout<<" What letter grade do you deserve? ";
+	cin>>grade;
+	cout<<" What is your age? ";
+	cin>>age;	
+	cout<<" Name: "<<name_last<<" , "<<name_first<<endl;
+	cout<<" Grade: "<< ++grade <<endl;
+	cout<<" Age: "<<age<<endl;
+	//C风格字符数组几种输入方法 对流中空格的处理， 结束符的区别
+	//cin.get .getline .read 的区别 
+	cout<<"_________________________________"<<endl<<endl;      
+	//2
+
 
 
 
