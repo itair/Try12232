2fc53bcdcc3695f3e3a3f1286eea7a4db724f98b
itair <itair@163.com>
2013-12-24 15:38:30 +0800

    第二题
    // string 代替 char[]的好处是安全性高；

--- Try12232/try.cpp
+++ Try12232/try.cpp
@@ -1,13 +1,11 @@
 //C++ Primer Plus 课后习题 p109 3.7 
 // by itAir
 // Try to use Google C++ programing style
-// dir2/foo2.h（优先位置，详情如下）
-//C 系统文件
-//        C++系统文件
-//        其他库头文件
-//        本项目内头文件
+
 #include "try.h"
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
 void main(){
@@ -16,27 +14,38 @@ void main(){
 	cout<<"Hello World!"<<endl;
 	cout<<"_________________________________"<<endl<<endl;        
 	//1
-	char name_first[20], name_last[10];
-	char grade;
-	int age;
-	cout<<" What is your first name? ";
-	cin.getline(name_first,20);
-	cout<<" What is your last name? ";
-	cin.getline(name_last,10);
-	cout<<" What letter grade do you deserve? ";
-	cin>>grade;
-	cout<<" What is your age? ";
-	cin>>age;	
-	cout<<" Name: "<<name_last<<" , "<<name_first<<endl;
-	cout<<" Grade: "<< ++grade <<endl;
-	cout<<" Age: "<<age<<endl;
+// 	char name_first[20], name_last[10];
+// 	char grade;
+// 	int age;
+// 	cout<<" What is your first name? ";
+// 	cin.getline(name_first,20);
+// 	cout<<" What is your last name? ";
+// 	cin.getline(name_last,10);
+// 	cout<<" What letter grade do you deserve? ";
+// 	cin>>grade;
+// 	cout<<" What is your age? ";
+// 	cin>>age;	
+// 	cout<<" Name: "<<name_last<<" , "<<name_first<<endl;
+// 	cout<<" Grade: "<< ++grade <<endl;
+// 	cout<<" Age: "<<age<<endl;
 	//C风格字符数组几种输入方法 对流中空格的处理， 结束符的区别
 	//cin.get .getline .read 的区别 
+	//get()不丢弃回车 getline不忽略空格 read只能读char
 	cout<<"_________________________________"<<endl<<endl;      
 	//2
-
-
-
+// 	const int ArSize = 20;
+// 	char name[ArSize];
+// 	char dessert[ArSize];
+  string name, dessert;
+  cout << "Enter your name: \n";
+  cin >> name;
+  cout << "Enter your favorite dessert: \n";
+  cin >> dessert;
+  cout << "I have some delicious "<< dessert;
+  cout << " for you, " << name << ".\n";
+  // string 代替 char[]的好处是安全性高；
+  cout<<"_________________________________"<<endl<<endl;   
+  //3
 
 
 	system("pause");
